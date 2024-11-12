5adbfaa4b5c4c96cf67552a87427df08b26dae0a
Admin <Admin@192.168.1.3>
2024-11-12 20:59:25 +0530

    By Vishal

--- B181/src/com/Demo.java
+++ B181/src/com/Demo.java
@@ -3,6 +3,11 @@ package com;
 public class Demo 
 {
 
+	public void m1()
+	{
+		System.out.println("M1 Method");
+	}
+	
 	public static void main(String[] args) 
 	{
 		System.out.println("Main Class");
